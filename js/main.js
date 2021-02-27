6bef39e7161d853b22d545988c69bbab34509463
bryanares <bryanares12@gmail.com>
2021-02-27 09:57:14 +0300

    added jQuery to the what we do section to the thumbnails

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	$("#design-hidden").click(function () {
+		$("#design-hidden").hide();
+		$("#design-showing").show();
+	});
+	$("#design-showing").click(function () {
+		$("#design-hidden").show();
+		$("#design-showing").hide();
+	});
+	$("#development-hidden").click(function () {
+		$("#development-hidden").hide();
+		$("#development-showing").show();
+	});
+	$("#development-showing").click(function () {
+		$("#development-hidden").show();
+		$("#development-showing").hide();
+	});
+	$("#projectmgt-hidden").click(function () {
+		$("#projectmgt-hidden").hide();
+		$("#projectmgt-showing").show();
+	});
+	$("#projectmgt-showing").click(function () {
+		$("#projectmgt-hidden").show();
+		$("#projectmgt-showing").hide();
+	});
+});
